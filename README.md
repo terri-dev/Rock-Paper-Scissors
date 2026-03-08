b18bc3ba132725d72ac6b4debcc76f0d793799c3
terri-dev <254127822+terri-dev@users.noreply.github.com>
2026-03-08 19:36:29 +0300

    Edit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Rock-Paper-Scissors
+
+This is a Rck Paper Scissors game played in the console. All you have to do is run playGame(rounds). Make sure to input the number of rounds you would like to play.
---
+++ script.js
@@ -0,0 +1,55 @@
+
+function playGame(rounds) {
+    let computerScore = 0;
+    let humanScore = 0;
+
+    let name = prompt('What is your name? ', 'Player')
+
+    for (let i=0; i<rounds; i++){
+        let round = i+1;
+        let humanSelection = getHumanChoice(round);
+        let computerSelection = getComputerChoice();
+        let score = playRound(humanSelection, computerSelection, round)
+        if (score){humanScore += 1}else if (score == false){computerScore += 1}
+        alert (`Computer = ${computerScore}, ${name} = ${humanScore}`)
+    }
+    let winner;
+    if (computerScore > humanScore){
+        winner = "Computer";
+    }else {
+        winner = name;
+    }
+    console.log(`The winner is ${winner}`)
+    alert(`The winner is ${winner}`)
+}
+
+function playRound(humanSelection, computerSelection, round){
+    if (humanSelection == "Rock" && computerSelection == "Scissors" || humanSelection == "Paper" && computerSelection == "Rock" || humanSelection == "Scissors" && computerSelection == "Paper") {
+        alert(`${humanSelection} beats ${computerSelection}. You win Round ${round}!`)
+        return true;
+    }else if (humanSelection == "Scissors" && computerSelection == "Rock" || humanSelection == "Rock" && computerSelection == "Paper" || humanSelection == "Paper" && computerSelection == "Scissors") {
+        alert(` ${computerSelection} beats ${humanSelection}. You lose Round ${round}!`)
+        return false;
+    }else {
+        alert("It's a draw.")
+        return;
+    }
+}
+
+function getHumanChoice(round) {
+    let input = prompt(`Round ${round}.\nMake a choice (Rock, Paper, Scissors): `, "Rock");
+    input = input.toUpperCase();
+    input = input.replace(input.slice(1, input.length), input.slice(1, input.length).toLowerCase())
+    return input;
+}
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3)
+    if (num == 1) {
+        return "Rock";
+    } else if (num == 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
